d9b45fd72a2629255420d6edd99afa683fa204a4
Sahil Makhijani <sahil.makhijani@gmail.com>
2023-02-05 00:53:51 +0530

    Add Play/Pause Functionality

--- lib/main.dart
+++ lib/main.dart
@@ -29,13 +29,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  int count = 0;
+  bool isStarted = false;
 
   @override
   Widget build(BuildContext context) {
@@ -51,16 +46,20 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              '$count',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: Icon(isStarted ? Icons.pause : Icons.play_arrow),
+        onPressed: () {
+          setState(() {
+            count++;
+            isStarted = !isStarted;
+          });
+        },
       ),
     );
   }
